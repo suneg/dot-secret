d427ff8928ce6c21262fc637a4ed158503e131af
Sune Gynthersen <sune.gynthersen@gmail.com>
2015-01-04 21:42:11 +0100

    fixed version of AES encrypted strings (now using random IV)

---
+++ password.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace DFDG.Util.Encryption {
+    public class StringCipher
+    {
+        // This constant is used to determine the keysize of the encryption algorithm.
+        private static int ivLength = 16;
+        private const int keysize = 256;
+
+        private static void GenerateRandomIv(byte[] iv) {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            rnd.NextBytes(iv);
+        }
+
+        public static string Encrypt(string plainText, string passPhrase)
+        {
+            byte[] initVectorBytes = new byte[ivLength];
+            GenerateRandomIv(initVectorBytes);
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using (PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null))
+            {
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                using (RijndaelManaged symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.Mode = CipherMode.CBC;
+                    using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                cryptoStream.FlushFinalBlock();
+                                byte[] cipherTextBytes = memoryStream.ToArray();
+                                byte[] ivAndCipherText = new byte[ivLength + cipherTextBytes.Length];
+                                System.Buffer.BlockCopy(initVectorBytes, 0, ivAndCipherText, 0, ivLength);
+                                System.Buffer.BlockCopy(cipherTextBytes, 0, ivAndCipherText, ivLength, cipherTextBytes.Length);
+
+
+                                return Convert.ToBase64String(ivAndCipherText);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string passPhrase)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+            byte[] iv = new byte[ivLength];
+
+            System.Buffer.BlockCopy(cipherTextBytes, 0, iv, 0, ivLength);
+
+            byte[] cipherTextBytesWithoutIv = new byte[cipherTextBytes.Length - ivLength];
+            System.Buffer.BlockCopy(cipherTextBytes, ivLength, cipherTextBytesWithoutIv, 0, cipherTextBytesWithoutIv.Length);
+
+            using(PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null))
+            {
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                using(RijndaelManaged symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.Mode = CipherMode.CBC;
+                    using(ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, iv))
+                    {
+                        using(MemoryStream memoryStream = new MemoryStream(cipherTextBytesWithoutIv))
+                        {
+                            using(CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                            {
+                                byte[] plainTextBytes = new byte[cipherTextBytesWithoutIv.Length];
+                                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class Bootstrap
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Please enter a string to encrypt:");
+            //string plaintext = Console.ReadLine();
+            for(int i = 0;i < 25; i++) 
+            {
+                var plaintext = "1Kamel2Pukler";
+                Console.WriteLine("");
+
+                Console.WriteLine("Your encrypted string is:");
+                string encryptedstring = StringCipher.Encrypt(plaintext, password);
+                Console.WriteLine(encryptedstring);
+                Console.WriteLine("");
+
+                //string encryptedstring = Console.ReadLine();
+
+                Console.WriteLine("Your decrypted string is:");
+                string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+                Console.WriteLine(decryptedstring);
+            }
+
+            Console.WriteLine("");
+
+            Console.ReadLine();
+        }
+    }
+}
